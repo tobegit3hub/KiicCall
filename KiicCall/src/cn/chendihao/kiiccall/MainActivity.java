56a309f72580d80b238c874762e82e257e741551
tobe <tobe@tobe.(none)>
2013-07-28 22:09:42 +0800

    add license

--- KiicCall/src/cn/chendihao/kiiccall/MainActivity.java
+++ KiicCall/src/cn/chendihao/kiiccall/MainActivity.java
@@ -9,26 +9,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         this.finish();
-        
-        
-		ImageView imageView = new ImageView(this);
-		imageView.setImageResource(R.drawable.heart);
+                
+	ImageView imageView = new ImageView(this);
+	imageView.setImageResource(R.drawable.heart);
 		
         Toast toast = new Toast(this);
         toast.setView(imageView);
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0, 0);
-        toast.show();
-        
+        toast.show();        
         
         String tobeNumber = "626796";  //637104
 		Intent intent = new Intent(Intent.ACTION_CALL);
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 KiicCall
 ========
 
-Simple Android telephone app for my dear Kiic
+## Introduction
+KiicCall is a really simple application for making a call. It's designed for my girlfriend, kiic, to call me. 
+
+## Implement
+Implement this telephone app is quite easy. 
+1. Just think about writing an app for your lover.
+2. Start an Android project.
+3. Learn about intent and Android telephone API.
+4. `Toast` anything you like.
+
